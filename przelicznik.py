44899b2639a4d207a49125450fe9c91406d5e043
Sim1234 <szymon.zmilczak@gmail.com>
2017-03-15 22:29:59 +0100

    prefixes

--- przelicznik.py
+++ przelicznik.py
@@ -1,5 +1,7 @@
 # coding: utf8
 from fractions import Fraction
+import typing
+
 
 class ValueType:
     pass
@@ -7,8 +9,8 @@ class ValueType:
 
 class Unit:
     value_type = ValueType
-    display_name = None
-    short_name = None
+    display_name = ''
+    short_name = ''
 
     @classmethod
     def from_absolute_value(cls, value: Fraction):
@@ -18,50 +20,123 @@ class Unit:
     def to_absolute_value(cls, value: Fraction):
         return value
 
-    def __init__(self, value: Fraction, is_absolute=False):
+    def __init__(self, value: typing.Any, is_absolute=False):
+        super(Unit, self).__init__()
         if not is_absolute:
             value = self.to_absolute_value(Fraction(value))
-        self.absolute_value = value
+        self.absolute_value = Fraction(value)
 
-    def convert_to(self, unit):
+    def convert_to(self, unit: 'Unit'):
         if self.value_type != unit.value_type:
             raise TypeError('Incompatible unit type')
         return unit(self.absolute_value, True)
 
+    def __float__(self):
+        return float(self.from_absolute_value(self.absolute_value))
+
     def __str__(self):
-        return "{0:.4f}{1}".format(float(self.from_absolute_value(self.absolute_value)), self.short_name)
+        return '{0:.20g}{1}'.format(float(self), self.short_name)
 
     def __repr__(self):
-        return "{0}({1})".format(self.__class__.__qualname__, self.absolute_value.__repr__())
+        return '{0}({1})'.format(type(self).__qualname__, self.absolute_value.__repr__())
 
 
 class Prefix(Unit):
-    multiplier = Fraction('1')
     display_name = ''
     short_name = ''
+    multiplier = Fraction('1')
+    unit = Unit
 
-    def __init__(self, unit: Unit):
-        self.unit = unit
+    @classmethod
+    def create_unit(cls, unit: Unit):
+        return type('{0}{1}'.format(cls.display_name.title(), unit.display_name.title()), (cls, unit), {
+            'display_name': '{0} {1}'.format(cls.display_name, unit.display_name),
+            'short_name': '{0}{1}'.format(cls.short_name, unit.short_name),
+            'unit': unit,
+        })
+
+    @staticmethod
+    def __new__(cls, *args, **kwargs):
+        if len(args) == 1 and isinstance(args[0], type) and issubclass(args[0], Unit):
+            return cls.create_unit(args[0])
+        # return super(cls).__new__(cls, *args, **kwargs)
+        return super().__new__(cls)
+        print("NEW", cls, args, kwargs, type(r))
 
     @classmethod
     def from_absolute_value(cls, value: Fraction):
-        return value * cls.multiplier
+        return cls.unit.from_absolute_value(value) / Fraction(cls.multiplier)
+
+    @classmethod
+    def to_absolute_value(cls, value: Fraction):
+        return cls.unit.to_absolute_value(value * Fraction(cls.multiplier))
+
+
+class Milli(Prefix):
+    display_name = 'Milli'
+    short_name = 'm'
+    multiplier = Fraction('0.001')
+
+
+class Micro(Prefix):
+    display_name = 'Micro'
+    short_name = 'μ'
+    multiplier = Fraction('0.000001')
+
+
+class Nano(Prefix):
+    display_name = 'Nano'
+    short_name = 'n'
+    multiplier = Fraction('0.000000001')
+
+
+class Pico(Prefix):
+    display_name = 'Pico'
+    short_name = 'p'
+    multiplier = Fraction('0.000000000001')
+
+
+class Kilo(Prefix):
+    display_name = 'Kilo'
+    short_name = 'k'
+    multiplier = Fraction('1000')
+
+
+class Mega(Prefix):
+    display_name = 'Mega'
+    short_name = 'M'
+    multiplier = Fraction('1000000')
+
+
+class Giga(Prefix):
+    display_name = 'Giga'
+    short_name = 'G'
+    multiplier = Fraction('1000000000')
+
+
+class Tera(Prefix):
+    display_name = 'Tera'
+    short_name = 'T'
+    multiplier = Fraction('100000000000000')
 
 
 class Temperature(ValueType):
     pass
+
+
 class Distance(ValueType):
     pass
 
+
 class Kelvin(Unit):
     value_type = Temperature
-    display_name = "Kelvin"
-    short_name = '°K'
+    display_name = 'Kelvin'
+    short_name = 'K'
 
 
 class Celsius(Unit):
     value_type = Temperature
-    display_name = "Celsius"
+    display_name = 'Celsius'
     short_name = '°C'
 
     @classmethod
@@ -72,10 +147,11 @@ class Celsius(Unit):
     def to_absolute_value(cls, value: Fraction):
         return value + Fraction('273.15')
 
+
 class Fahrenheit(Unit):
     value_type = Temperature
-    display_name = "Fahrenheit"
-    short_name = "°F"
+    display_name = 'Fahrenheit'
+    short_name = '°F'
 
     @classmethod
     def from_absolute_value(cls, value: Fraction):
@@ -86,13 +162,10 @@ class Fahrenheit(Unit):
         return Fraction('5/9') * (value - Fraction('32')) + Fraction('273.15')
 
 
-
-
-
-    class metr(Unit):
-        value_type = Distance
-        display_name = "metr"
-        short_name = "m"
+class Meter(Unit):
+    value_type = Distance
+    display_name = "Meter"
+    short_name = "m"
 
 
 if __name__ == '__main__':
@@ -114,4 +187,14 @@ if __name__ == '__main__':
     print(F4.convert_to(Celsius))
     print(F4.convert_to(Kelvin))
     print(F4.convert_to(Fahrenheit))
+    print()
+    mk = Milli(Kelvin)
+    print(K4.convert_to(Milli(Kelvin)))
+    print(K4.convert_to(Milli(Kelvin)).convert_to(Kelvin))
+    print(C4)
+    print(C4.convert_to(Milli(Celsius)))
+    print(F4)
+    print(F4.convert_to(Pico(Kelvin)))
+    print(F4.convert_to(Tera(Kelvin)))
+
 
